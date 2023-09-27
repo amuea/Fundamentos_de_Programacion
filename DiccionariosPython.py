9203dace93e487677940eaa3862501c5111aed17
USER <am.torresd@uea.edu.ec>
2023-09-26 23:29:46 -0500

    Trabajando con Diccionarios en Python

---
+++ DiccionariosPython.py
@@ -0,0 +1,10 @@
+# Crear un Diccionario
+
+informacion_personal = {"nombre": "Lisbeth", "edad": 23, "ciudad": "Lago Agrio", "profesion": "Biologa", "telefono": 0989738320}
+
+informacion_personal["ciudad"] = "Quito"
+informacion_personal["profesion"] = "Ciencias"
+
+del(informacion_personal["edad"])
+
+print(informacion_personal)
